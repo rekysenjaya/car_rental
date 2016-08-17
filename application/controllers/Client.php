7311f56913384499c890262347e6d9b51858da82
Reky Senjaya <rekysenjaya@gmail.com>
2016-08-17 22:29:06 +0700

    update

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -9,10 +9,10 @@ class Client extends CI_Controller {
   }
 
   public function index($id = null) {
-    $this->load->model('Client');
+    $this->load->model('Client_model');
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method == 'GET') {
-      $resp = $this->Client->client_all_data();
+      $resp = $this->Client_model->client_all_data();
       json_output(200, $resp);
     } elseif ($method == 'POST') {
       $params = json_decode(file_get_contents('php://input'), TRUE);
@@ -21,7 +21,7 @@ class Client extends CI_Controller {
         $resp = array('status' => 400, 'message' => 'Name & Gender can\'t empty');
       } else {
         $respStatus = 200;
-        $resp = $this->Client->client_create_data($params);
+        $resp = $this->Client_model->client_create_data($params);
       }
       json_output($respStatus, $resp);
     } elseif ($method == 'PUT') {
@@ -30,10 +30,10 @@ class Client extends CI_Controller {
         $respStatus = 400;
         $resp = array('status' => 400, 'message' => 'Name & Gender can\'t empty');
       } else {
-        $resp = $this->Client->client_update_data($id, $params);
+        $resp = $this->Client_model->client_update_data($id, $params);
       }
     } elseif ($method == 'DELETE') {
-      $resp = $this->Client->client_delete_data($id);
+      $resp = $this->Client_model->client_delete_data($id);
     } else {
       json_output(400, array('status' => 400, 'message' => 'Bad request.'));
     }
